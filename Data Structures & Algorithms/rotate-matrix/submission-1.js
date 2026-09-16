ec6b1cbb21cd55f315f62312ee106b6659e1cb62
Brennan LeSuer <53055814+blesuer0729@users.noreply.github.com>
2026-09-15 23:35:18 -0400

    Add: rotate-matrix - submission-1

---
+++ Data Structures & Algorithms/rotate-matrix/submission-1.js
@@ -0,0 +1,17 @@
+class Solution {
+    /**
+     * @param {number[][]} matrix
+     * @return {void}
+     */
+    rotate(matrix) {
+        matrix.reverse();
+
+        console.log(matrix);
+        
+        for (let row = 0; row < matrix.length; row++) {
+            for (let column = row; column < matrix[row].length; column++) {
+                [matrix[column][row], matrix[row][column]] = [matrix[row][column], matrix[column][row]];
+            }
+        }
+    }
+}
